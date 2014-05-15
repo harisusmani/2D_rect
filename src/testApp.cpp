3ac53a19c4fdb0073091208b69bc854328974dca
harisusmani <me@harisusmani.com>
2014-05-14 21:54:49 -0400

    Cleaned up the Basic Example
    Taken out troubleshooting I/O, Comments, Extra Functions (other
    hypotheses I tried for better results)

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -13,26 +13,24 @@ bool comparator ( const mypair& l, const mypair& r){
 void testApp::setup(){
     talk=0; //Troubleshooting
 
-    //filename="flickr/window.jpg";
-    cout << "Filename: ";
-	cin >> filename;
+    filename="dc_1.jpg"; //Input Image
+    //filename="floor_hunt.jpg";
+    /*cout << "Filename: ";
+	cin >> filename;*/
+
 	my_image.loadImage(filename);
 	filename.insert(filename.size()-4,"_ofxOUT");
 
-	//Import EXIF Data, Find a Good Library//
-	//focal_length=22; //Canon EOS M";
-	//sensor_width=22.3;
+	//Import EXIF Data//...
+        //Note: This is yet to be Implemented. Please enter Focal
+        //Length and Sensor Width manually in same unit (mm).
+	focal_length=22; //Canon EOS M for dc_1.jpg
+	sensor_width=22.3;
 
-	//focal_length=4; //Nexus 5
+	//focal_length=4; //Nexus 5, for floor_hunt.jpg
 	//sensor_width=4.59;
-    cout << endl << "Focal: ";
-    cin >> focal_length;
 
-    cout << endl << "Sensor Width: ";
-    cin >> sensor_width;
-    //focal_length=18; //Seasick
-	//sensor_width=23.6;
-	//EXIF Data Completed//
+	//...EXIF Data Completed//
 
     resize_image=1; //To Enable or Disable Resize
 
@@ -93,10 +91,9 @@ void testApp::setup(){
     cout << "LSD Values: " << lsd_out->values[0] << " " << lsd_out->values[1] <<" " <<  lsd_out->values[2] <<" " <<  lsd_out->values[3] <<" " <<  lsd_out->values[4] <<" " <<  lsd_out->values[5] << "\n";
     }
 
-    //SORTING WHICH RETURNS INDEX TOO!
-    //http://stackoverflow.com/questions/1577475/c-sorting-and-keeping-track-of-indexes
+    //Sorting in (Value, Index) pairs
+        //http://stackoverflow.com/questions/1577475/c-sorting-and-keeping-track-of-indexes
     std::vector<mypair> line_lengths;
-    //double distance;
     double sqd_distance;
 
     mesh.setMode(OF_PRIMITIVE_LINES);
@@ -111,19 +108,18 @@ void testApp::setup(){
         y1=lsd_out->values[j+1];
         x2=lsd_out->values[j+2];
         y2=lsd_out->values[j+3];
-        //distance=sqrt(pow(lsd_out->values[j+2]-lsd_out->values[j],2) + pow(lsd_out->values[j+3]-lsd_out->values[j+1],2));
         sqd_distance=(x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
 
         line_lengths.push_back(make_pair(sqd_distance,j));
 
-        //To Draw as Primitive Lines //
-        first.set(x2,y2,0.0);
+        //To Draw as Primitive Lines//
+        /*first.set(x2,y2,0.0);
         second.set(x1,y1,0.0);
         mesh.addVertex(first);
         mesh.addColor(ofFloatColor(1.0, 0.0, 0.0));
         mesh.addVertex(second);
-        mesh.addColor(ofFloatColor(1.0, 0.0, 0.0));
-        //Lines Added, will be drawn //
+        mesh.addColor(ofFloatColor(1.0, 0.0, 0.0));*/
+        //Lines Added, will be drawn//
        }
     sort(line_lengths.begin(),line_lengths.end());
     reverse(line_lengths.begin(), line_lengths.end());
@@ -131,23 +127,20 @@ void testApp::setup(){
     if (talk) cout << line_lengths[0].first << " " << line_lengths[0].second << " " << line_lengths[1].first << " " << line_lengths[1].second << "\n";
 
     unsigned int maxlines=700;
-    //unsigned int max2=floor(lsd_out->size*0.7);
     unsigned int no_of_lines=min(lsd_out->size,maxlines);
     //Store these Lines pairs in a Matrix, in descending order of Distance
     cout << "Number of Lines: " << no_of_lines << "\n";
-    L.resize(4); //Height
+    L.resize(4);
     for (int i = 0; i < 4; ++i){
         L[i].resize(no_of_lines);
     }
 
     for (int j=0; j<no_of_lines; j++){
-        L[0][j] = lsd_out->values[line_lengths[j].second];//-center.x; //Move Origin to the Principle Point too
-        L[1][j] = lsd_out->values[(line_lengths[j].second)+1];//-center.y;  //...Required for Vector Form of Lines
-        L[2][j] = lsd_out->values[(line_lengths[j].second)+2];//-center.x;
-        L[3][j] = lsd_out->values[(line_lengths[j].second)+3];//-center.y;
+        L[0][j] = lsd_out->values[line_lengths[j].second];
+        L[1][j] = lsd_out->values[(line_lengths[j].second)+1];
+        L[2][j] = lsd_out->values[(line_lengths[j].second)+2];
+        L[3][j] = lsd_out->values[(line_lengths[j].second)+3];
     }
-
-    //GAP FILLING (Skipped)
 
     //LINE EXTENSION
     double extension_fac=.15; //For one side of the line
@@ -204,20 +197,14 @@ void testApp::setup(){
         y1=L[1][j];
         x2=L[2][j];
         y2=L[3][j];
-        //To Draw as Primitive Lines //
-        first.set(x2,y2,0.0);
-        second.set(x1,y1,0.0);
-        mesh.addVertex(first);
-        mesh.addColor(ofFloatColor(0.0, 1.0, 0.0));
-        mesh.addVertex(second);
-        mesh.addColor(ofFloatColor(0.0, 1.0, 0.0));
-        L[2][j]-=center.x;
+
+        L[2][j]-=center.x; //Move Origin to the Principle Point
         L[3][j]-=center.y;
         L[0][j]-=center.x;
         L[1][j]-=center.y;
     }
 
-    //Finding Adjacent Lines
+    //Finding ADJACENT Lines//
     bool adjflag=1;
     std::vector<int> ar; //To hold Adjacent Row Values
     std::vector<int> ac; //To hold Adjacent Column Values
@@ -227,6 +214,7 @@ void testApp::setup(){
     double athreshadj=10;
 
     std::vector<std::vector<bool> > adj; //Line x Line Inf Matrix Initialization, adj
+                                            //Not Used at the Moment.
     adj.resize(no_of_lines); //Height
     for (int i = 0; i < no_of_lines; ++i){
         adj[i].resize(no_of_lines);
@@ -239,7 +227,7 @@ void testApp::setup(){
     ofVec2f v1,v2,x;
     athreshadj=abs(cos((athreshadj*PI)/180.0));
     for (int i = 0; i < no_of_lines; ++i){
-        for (int j = i+1; j < no_of_lines; ++j){ //Everyline infront
+        for (int j = i+1; j < no_of_lines; ++j){ //Everyline in-front
             v1.set(L[0][i]-L[2][i],L[1][i]-L[3][i]);
             v2.set(L[0][j]-L[2][j],L[1][j]-L[3][j]);
             v1.normalize();
@@ -253,25 +241,8 @@ void testApp::setup(){
                    adj[j][i]=adj[i][j] || adj[j][i];
                    if (adj[i][j])
                    {
-                      //cout << "i=" << i <<"  j=" << j << "\n";
                       ar.push_back(i);
                       ac.push_back(j);
-
-                      //TEST
-                     /* first.set(L[0][i],L[1][i],0.0);
-                      second.set(L[2][i],L[3][i],0.0);
-                      mesh.addVertex(first);
-                      mesh.addColor(ofFloatColor(0.0, 0.0, 1.0));
-                      mesh.addVertex(second);
-                      mesh.addColor(ofFloatColor(0.0, 0.0, 1.0));
-
-                      first.set(L[0][j],L[1][j],0.0);
-                      second.set(L[2][j],L[3][j],0.0);
-                      mesh.addVertex(first);
-                      mesh.addColor(ofFloatColor(0.0, 0.0, 1.0));
-                      mesh.addVertex(second);
-                      mesh.addColor(ofFloatColor(0.0, 0.0, 1.0));*/
-                      //------//
                    }
                }
             }
@@ -281,7 +252,7 @@ void testApp::setup(){
     else
     {
     for (int i = 0; i < no_of_lines; ++i){ // nC2 Pairs
-        for (int j = i+1; j < no_of_lines; ++j){ //Everyline infront
+        for (int j = i+1; j < no_of_lines; ++j){
             ar.push_back(i);
             ac.push_back(j);
             }
@@ -289,9 +260,9 @@ void testApp::setup(){
     }
     cout << "No. of Pairs: "<< ac.size() << "\n";
 
-    //Adjacent Matrix ENDS
+    //Adjacent Matrix FOUND//
 
-    //Convert Line Segments to vector format for Rectification //
+    //Convert Line Segments to vector format for Rectification//
     std::vector<std::vector<double> > L_vec; //Line's Vector Form
     L_vec.resize(3); //Height
     for (int i = 0; i < 3; ++i){
@@ -324,15 +295,15 @@ void testApp::setup(){
     unsigned int maxTrials=400; //min(PairsC2*5,200);
     cout << "RANSAC: No. of Trials = " << maxTrials << endl;
     unsigned int trialcount=0;
-    unsigned int r1, r2, r3, r4; //Pick the 4 lines I like to test. //0,1,4,3 WORK!!
+    unsigned int r1, r2, r3, r4;
     unsigned int r_ind1, r_ind2;
     column_vector modelX;
     std::vector<int> arIn; //To hold Adjacent Row Values
-    std::vector<int> acIn;
-    double thresh=0.001;//0.001; //0.00000001;//0.01;
+    std::vector<int> acIn; //To hold Adjacent Column Values
+    double thresh=0.001;
 
-    std::vector<int> Best_arIn; //To hold Adjacent Row Values
-    std::vector<int> Best_acIn;
+    std::vector<int> Best_arIn; //Best Adjacent Row Values
+    std::vector<int> Best_acIn; //Best Adjacent Column Values
     unsigned int Bestscore=0; //Number of Inliers
     unsigned int score=0;
     column_vector Best_modelX;
@@ -368,22 +339,9 @@ void testApp::setup(){
     cout << "RANSAC Done." << endl;
     cout << "No. of Inliers: "<< Bestscore<<endl;
 
-    //Skipping RANSAC, Testing Solver First
-    //Pick two good lines and solve for Homography, Apply Homography to Image
-                //Pick the 4 lines I like to test. //0,1,4,3 WORK!!
-                /*r1=0;
-                r2=1;
-                r3=4;
-                r4=3;
-                Best_modelX=fitFunc4Lines(L_vec, r1, r2, r3, r4, f);*/
-
-    //column_vector solution= fitFunc4Lines(L_vec, r1, r2, r3, r4, f);
-
     column_vector solution=Best_modelX;
 
     if (talk) cout << "cost_function solution:\n" << Best_modelX << endl;
-
-    //column_vector solution=fitFuncNLines(Best_modelX, L_vec, Best_arIn, Best_acIn, f); //Fitting Function with Adjacency Matrix (Inliers)
     cout << "cost_function solution:\n" << solution << endl;
 
 
@@ -391,19 +349,13 @@ void testApp::setup(){
     ofMatrix4x4 R=ofMatrix4x4::newRotationMatrix(solution(0)*180.0/PI, ofVec3f(-1, 0, 0), solution(1)*180.0/PI, ofVec3f(0, -1, 0), 0, ofVec3f(0, 0, -1));
     double m[3][3] = {{R(0,0), R(0,1), R(0,2)}, {R(1,0), R(1,1), R(1,2)}, {R(2,0), R(2,1), R(2,2)}};
     cv::Mat R_mat = cv::Mat(3, 3, CV_64F, m);
-    //cout << "R_mat" << R_mat <<endl;
 
     cv::Mat K_mat = (cv::Mat_<double>(3,3)<< f,0.0,0.0,0.0,f,0.0,0.0,0.0,1.0);
-    //cout << "K" << K_mat <<endl;
     cv::Mat K_c= K_mat.clone();
     K_c=K_c.inv();
-    //cout << "Kinv" << K_c <<endl;
 
     cv::Mat C = (cv::Mat_<double>(3,3)<< 1,0,-center.x,0,1,-center.y,0,0,1);
-    //cout << "C" << C <<endl;
     cv::Mat H=K_mat*R_mat*K_c*C;
-
-    //cout << "H before Transform" << H << endl;
 
     //Calclating Resultant Translation and Scale
     std::vector<Point2f> Ref_c;
@@ -420,7 +372,6 @@ void testApp::setup(){
     Ref_c[3].y=double(my_image.height);
 
     perspectiveTransform(Ref_c, Ref_c_out, H);
-    //cout << endl << "Ref Out: " << Ref_c_out << endl;
 
     if (talk) cout << "Ref Out New: " << Ref_c_out << endl;
 
@@ -437,23 +388,7 @@ void testApp::setup(){
     Ref_c_out[3].x=Ref_c_out[3].x/scale_fac;
     Ref_c_out[3].y=Ref_c_out[3].y/scale_fac;
 
-    /*Point2f NCenter;
-    NCenter.x=(max(Ref_c_out[1].x,Ref_c_out[2].x)-min(Ref_c_out[0].x,Ref_c_out[3].x))/2.0;
-    NCenter.y=(max(Ref_c_out[2].y,Ref_c_out[3].y)-min(Ref_c_out[0].y,Ref_c_out[1].y))/2.0;
-    cout << "New Center: " << NCenter << endl;
-    NCenter.x-=500;
-    NCenter.y-=500;
-
-    Ref_c_out[0].x=Ref_c_out[0].x-NCenter.x;
-    Ref_c_out[0].y=Ref_c_out[0].y-NCenter.y;
-    Ref_c_out[1].x=Ref_c_out[1].x-NCenter.x; //OR Find new Center and bring to Center of Canvas!
-    Ref_c_out[1].y=Ref_c_out[1].y-NCenter.y;
-    Ref_c_out[2].x=Ref_c_out[2].x-NCenter.x;
-    Ref_c_out[2].y=Ref_c_out[2].y-NCenter.y;
-    Ref_c_out[3].x=Ref_c_out[3].x-NCenter.x;
-    Ref_c_out[3].y=Ref_c_out[3].y-NCenter.y;*/
-
-    Ref_c_out[1].x=Ref_c_out[1].x-Ref_c_out[0].x; //OR Find new Center and bring to Center of Canvas!
+    Ref_c_out[1].x=Ref_c_out[1].x-Ref_c_out[0].x;
     Ref_c_out[1].y=Ref_c_out[1].y-Ref_c_out[0].y;
     Ref_c_out[2].x=Ref_c_out[2].x-Ref_c_out[0].x;
     Ref_c_out[2].y=Ref_c_out[2].y-Ref_c_out[0].y;
@@ -471,8 +406,11 @@ void testApp::setup(){
 	dst_img.create(src_img.size(), src_img.type());
 
 	cv::warpPerspective(src_img, dst_img, H, src_img.size(), cv::INTER_LINEAR);
+
     //OpenCV to OF
     my_image.setFromPixels((unsigned char *) IplImage(dst_img). imageData,dst_img.size().width, dst_img.size().height,OF_IMAGE_COLOR);
+
+    cout << endl <<"Press any key to Save Output Image." << endl;
 }
 
 //--------------------------------------------------------------
@@ -546,7 +484,7 @@ testApp::column_vector testApp::fitFunc4Lines(std::vector<std::vector<double> > 
         find_min_bobyqa(cost_function(L_vec, r1, r2, r3, r4, f),
                         starting_point,
                         5,    // number of interpolation points
-                        dlib::uniform_matrix<double>(2,1, 0),  // lower bound constraint
+                        dlib::uniform_matrix<double>(2,1, -PI/2),  // lower bound constraint
                         dlib::uniform_matrix<double>(2,1, PI/2),   // upper bound constraint
                         PI/10,    // initial trust region radius
                         1e-100,  // stopping trust region radius
@@ -606,87 +544,4 @@ void testApp::distFunc(std::vector<std::vector<double> >L_vec,testApp::column_ve
             }
         }
     }
-}
-
-unsigned int testApp::distFunc2(std::vector<std::vector<double> >L_vec,testApp::column_vector modelX,float f,double thresh,std::vector<int> & arIn,std::vector<int> & acIn, std::vector<int> ar, std::vector<int> ac){
-    //MAKE ROTATION MATRIX
-    ofMatrix4x4 R=ofMatrix4x4::newRotationMatrix(modelX(0)*180.0/PI, ofVec3f(-1, 0, 0), modelX(1)*180.0/PI, ofVec3f(0, -1, 0), 0, ofVec3f(0, 0, -1));
-    double m[3][3] = {{R(0,0), R(0,1), R(0,2)}, {R(1,0), R(1,1), R(1,2)}, {R(2,0), R(2,1), R(2,2)}};
-    cv::Mat R_mat = cv::Mat(3, 3, CV_64F, m);
-
-    cv::Mat K_mat = (cv::Mat_<double>(3,3)<< f,0.0,0.0,0.0,f,0.0,0.0,0.0,1.0);
-
-    cv::Mat K_c=K_mat.clone();
-    K_c=K_c.inv();
-    R_mat=R_mat.t();
-    cv::Mat Hinv=K_mat*R_mat*K_c;
-
-    double L_vec_D[3][L_vec[0].size()];
-    for (int i = 0; i < L_vec[0].size(); ++i){
-        L_vec_D[0][i]=L_vec[0][i];
-        L_vec_D[1][i]=L_vec[1][i];
-        L_vec_D[2][i]=L_vec[2][i];
-    }
-
-    cv::Mat L_vec_M=cv::Mat(3, L_vec[0].size(), CV_64F, L_vec_D);
-
-    Hinv=Hinv.t();
-    cv::Mat Lp=Hinv*L_vec_M;
-    Lp.resize(2);
-
-    double mag;
-    for (int i=0; i<L_vec[0].size(); i++)
-    {
-        mag=(Lp.at<double>(0,i)*Lp.at<double>(0,i))+(Lp.at<double>(1,i)*Lp.at<double>(1,i));
-        mag=sqrt(mag);
-        Lp.at<double>(0,i)=Lp.at<double>(0,i)/mag;
-        Lp.at<double>(1,i)=Lp.at<double>(1,i)/mag;
-    }
-
-    cv::Mat Lp_T=Lp.clone();
-    Lp_T=Lp_T.t();
-    cv::Mat C=Lp_T*Lp;
-    multiply(C, C, C, 1.0);
-
-    unsigned int score=0;
-
-    for (int i=0; i<L_vec[0].size(); i++)
-    {
-        for (int j=0; j<L_vec[0].size(); j++)
-        {
-            if (C.at<double>(i,j)<=thresh)
-            {
-                score+=1;
-            }
-        }
-    }
-
-    int i,j;
-    for (int x=0; x<ar.size(); x++)
-    {
-        i=ar[x];
-        j=ac[x];
-            if (C.at<double>(i,j)<=thresh)
-            {
-                arIn.push_back(i);
-                acIn.push_back(j);
-            }
-    }
-
-    return score;
-}
-
-testApp::column_vector testApp::fitFuncNLines(testApp::column_vector init_x, std::vector<std::vector<double> > L_vec, std::vector<int> Best_arIn, std::vector<int> Best_acIn, float f){
-        column_vector starting_point(2);
-        starting_point = init_x;
-        find_min_bobyqa(cost_functionNLines(L_vec, Best_arIn, Best_acIn, f),
-                        starting_point,
-                        5,    // number of interpolation points
-                        dlib::uniform_matrix<double>(2,1, 0),  // lower bound constraint
-                        dlib::uniform_matrix<double>(2,1, PI/2),   // upper bound constraint
-                        PI/10,    // initial trust region radius
-                        1e-100,  // stopping trust region radius
-                        2000    // max number of objective function evaluations
-        );
-    return starting_point;
 }
--- src/testApp.h
+++ src/testApp.h
@@ -34,7 +34,7 @@ class testApp : public ofBaseApp{
 		ofImage output_img;
 		string filename;
 		bool talk;
-		//EXIF
+
 		float focal_length;
 		float sensor_width;
 
@@ -53,8 +53,6 @@ class testApp : public ofBaseApp{
 
         column_vector fitFunc4Lines(std::vector<std::vector<double> > L_vec,unsigned int r1,unsigned int r2,unsigned int r3,unsigned int r4, float f);
         void distFunc(std::vector<std::vector<double> >L_vec,testApp::column_vector modelX,float f,double thresh,std::vector<int> & arIn,std::vector<int> & acIn);
-        unsigned int distFunc2(std::vector<std::vector<double> >L_vec,testApp::column_vector modelX,float f,double thresh,std::vector<int> & arIn,std::vector<int> & acIn, std::vector<int> ar, std::vector<int> ac);
-        column_vector fitFuncNLines(testApp::column_vector init_x, std::vector<std::vector<double> > L_vec, std::vector<int> Best_arIn, std::vector<int> Best_acIn, float f);
 };
 
 class cost_function
@@ -92,7 +90,6 @@ public:
         Lp_t=Lp_t.t();
 
         cv::Mat C=Lp_t*Lp;
-        //cout << C << endl;
         C=abs(C);
 
         double total_cost=C.at<double>(2,0)+C.at<double>(3,1)+C.at<double>(0,2)+C.at<double>(1,3);
@@ -103,83 +100,4 @@ public:
 private:
     cv::Mat L;
     cv::Mat K_mat;
-};
-
-class cost_functionNLines
-{
-public:
-
-    cost_functionNLines (std::vector<std::vector<double> > L_vec_in, std::vector<int> arIn_in, std::vector<int> acIn_in, double f)
-    { //RUN ALL INITIALIZATIONS HERE!
-
-        L_vec=L_vec_in;
-        arIn=arIn_in;
-        acIn=acIn_in;
-
-        double L_vec_D[3][L_vec[0].size()];
-        for (int i = 0; i < L_vec[0].size(); ++i){
-            L_vec_D[0][i]=L_vec[0][i];
-            L_vec_D[1][i]=L_vec[1][i];
-            L_vec_D[2][i]=L_vec[2][i];
-        }
-
-        L=cv::Mat(3, L_vec[0].size(), CV_64F, L_vec_D);
-
-        K_mat = (cv::Mat_<double>(3,3)<< f,0.0,0.0,0.0,f,0.0,0.0,0.0,1.0);
-
-    }
-
-    double operator() ( const testApp::column_vector& arg) const //THIS WILL BE CALLED REPEATREDLY!
-    {
-        //MAKE ROTATION MATRIX
-        ofMatrix4x4 R=ofMatrix4x4::newRotationMatrix(arg(0)*180.0/PI, ofVec3f(-1, 0, 0), arg(1)*180.0/PI, ofVec3f(0, -1, 0), 0, ofVec3f(0, 0, -1));
-        double m[3][3] = {{R(0,0), R(0,1), R(0,2)}, {R(1,0), R(1,1), R(1,2)}, {R(2,0), R(2,1), R(2,2)}};
-        cv::Mat R_mat = cv::Mat(3, 3, CV_64F, m);
-
-        cv::Mat K_c=K_mat.clone();
-        K_c=K_c.inv();
-        R_mat=R_mat.t();
-        cv::Mat Hinv=K_mat*R_mat*K_c;
-
-        Hinv=Hinv.t();
-        cv::Mat Lp=Hinv*L;
-        Lp.resize(2);
-
-        double mag;
-        for (int i=0; i<L_vec[0].size(); i++)
-        {
-            mag=(Lp.at<double>(0,i)*Lp.at<double>(0,i))+(Lp.at<double>(1,i)*Lp.at<double>(1,i));
-            mag=sqrt(mag);
-            Lp.at<double>(0,i)=Lp.at<double>(0,i)/mag;
-            Lp.at<double>(1,i)=Lp.at<double>(1,i)/mag;
-        }
-
-        cv::Mat Lp_t=Lp.clone();
-        Lp_t=Lp_t.t();
-
-        cv::Mat C=Lp_t*Lp;
-        multiply(C, C, C, 1.0);
-
-        cv::Mat IC = cv::Mat::eye(L_vec[0].size(), L_vec[0].size(), CV_64F)*1;
-        C=C-IC;
-
-        double total_cost=0;
-
-        for (int i=0; i<arIn.size(); i++)
-        {
-            if (not isnan(C.at<double>(arIn[i],acIn[i])))
-            {
-            total_cost=total_cost + C.at<double>(arIn[i],acIn[i]);
-            }
-        }
-        // cout << "Total Cost: " << total_cost << endl;
-        return(total_cost);
-    }
-
-private:
-    cv::Mat L;
-    cv::Mat K_mat;
-    std::vector<std::vector<double> > L_vec;
-    std::vector<int> arIn;
-    std::vector<int> acIn;
 };
